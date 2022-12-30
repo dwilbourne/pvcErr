ed84ff8ecd0cf616cec8006e40ead2082da6bd28
dougw <dougwilbourne@gmail.com>
2022-12-30 16:06:21 -0500

    added pvcStruct to ErrConfig

--- src/err/ErrConfig.php
+++ src/err/ErrConfig.php
@@ -9,38 +9,40 @@ declare(strict_types=1);
 namespace pvc\err;
 
 use Exception;
+use pvc\interfaces\err\ErrConfigInterface;
 
 /**
  * Class ErrConfig
  */
 class ErrConfig implements ErrConfigInterface
 {
-	public const LIBRARY_CODES = [
-		"pvcExceptions" => 1000,
-		"pvcBrandedStockExceptions" => 1002,
-	];
+    public const LIBRARY_CODES = [
+        "pvcExceptions" => 1001,
+        "pvcBrandedStockExceptions" => 1002,
+        "pvcStruct" => 1003,
+    ];
 
-	/**
-	 * createExceptionCode
-	 * @param string $libraryName
-	 * @param int $localExceptionCode
-	 * @return int
-	 * @throws Exception
-	 */
-	public static function createExceptionCode(string $libraryName, int $localExceptionCode): int
-	{
-		$globalPrefix = (string)self::getLibraryCode($libraryName);
-		return (int)($globalPrefix . $localExceptionCode);
-	}
+    /**
+     * createExceptionCode
+     * @param string $libraryName
+     * @param int $localExceptionCode
+     * @return int
+     * @throws Exception
+     */
+    public static function createExceptionCode(string $libraryName, int $localExceptionCode): int
+    {
+        $globalPrefix = (string)self::getLibraryCode($libraryName);
+        return (int)($globalPrefix . $localExceptionCode);
+    }
 
-	/**
-	 * getLibraryCode
-	 * @param string $libraryName
-	 * @return int|null
-	 * @throws Exception
-	 */
-	public static function getLibraryCode(string $libraryName): ?int
-	{
-		return self::LIBRARY_CODES[$libraryName] ?? null;
-	}
+    /**
+     * getLibraryCode
+     * @param string $libraryName
+     * @return int|null
+     * @throws Exception
+     */
+    public static function getLibraryCode(string $libraryName): ?int
+    {
+        return self::LIBRARY_CODES[$libraryName] ?? null;
+    }
 }
