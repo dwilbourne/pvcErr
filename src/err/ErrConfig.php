a6c874f67a1590f6c138b1a1eee21ec399be33bc
dougw <dougwilbourne@gmail.com>
2023-01-17 09:42:59 -0500

    changed pvcFilesys to pvcFileTree in config

--- src/err/ErrConfig.php
+++ src/err/ErrConfig.php
@@ -21,7 +21,7 @@ class ErrConfig implements ErrConfigInterface
         "pvcBrandedStockExceptions" => 1002,
         "pvcList" => 1003,
         "pvcTree" => 1004,
-	    "pvcFilesys" => 1005,
+	    "pvcFileTree" => 1005,
     ];
 
     /**
